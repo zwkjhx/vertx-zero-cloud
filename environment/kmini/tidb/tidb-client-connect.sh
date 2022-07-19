8f9b58496b36d55099e2039408bd3872fa1cff3f
Lang <silentbalanceyh@126.com>
2022-07-19 15:58:00 +0800

    Secret

--- environment/kmini/tidb/tidb-client-connect.sh
+++ environment/kmini/tidb/tidb-client-connect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-kubectl port-forward -n zero-cloud-hotel svc/basic-tidb 4000:4000 > log-tidb-4000.log &
-kubectl port-forward -n zero-cloud-hotel svc/basic-grafana 1202:3000 > log-tidb-pf1202.log &
-kubectl port-forward -n zero-cloud-hotel svc/basic-prometheus 1203:9090 &> log-tidb-pf1203.log &
+kubectl port-forward -n $ZC_NS svc/$ZC_TIDB_CLUSTER-tidb 4000:4000 > log-tidb-4000.log &
+kubectl port-forward -n $ZC_NS svc/$ZC_TIDB_NAME-grafana 1202:3000 > log-tidb-pf1202.log &
+kubectl port-forward -n $ZC_NS svc/$ZC_TIDB_NAME-prometheus 1203:9090 &> log-tidb-pf1203.log &
--- environment/kmini/tidb/tidb-kube-install-initialize.sh
+++ environment/kmini/tidb/tidb-kube-install-initialize.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+kubectl create secret generic aeon-secret --from-literal=root=pl,okmijn123 --namespace=$ZC_NS
 kubectl -n $ZC_NS apply -f config-tidb-initialize.yaml
