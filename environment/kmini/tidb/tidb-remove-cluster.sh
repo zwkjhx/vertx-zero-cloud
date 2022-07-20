b738cfa57025977dcddf7c31f01473ba33274393
Lang <silentbalanceyh@126.com>
2022-07-20 22:37:24 +0800

    Script

--- environment/kmini/tidb/tidb-remove-cluster.sh
+++ environment/kmini/tidb/tidb-remove-cluster.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-kubectl delete tidbmonitor $ZC_TIDB-monitor -n $ZK_NS && \
-kubectl delete tc $ZC_TIDB -n $ZK_NS && \
-kubectl delete pvc -n $ZK_NS -l app.kubernetes.io/instance=basic,app.kubernetes.io/managed-by=tidb-operator
+kubectl delete tidbmonitor $ZC_TIDB-monitor -n $ZK_NS
+kubectl delete tc $ZC_TIDB -n $ZK_NS
+kubectl delete tidbinitializer $ZC_TIDB-initializer -n $ZK_NS
+kubectl delete pvc -n $ZK_NS -l app.kubernetes.io/instance=$ZC_TIDB,app.kubernetes.io/managed-by=tidb-operator
