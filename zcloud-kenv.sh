dfa23fcd47be22604d6a560478342adaf9abb34a
Lang <silentbalanceyh@126.com>
2022-07-21 06:49:38 +0800

    Upgrade

--- zcloud-kenv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "## >>> Zero Cloud Environment >>>" >> ~/.zshrc
-echo "export ZERO_CLOUD=$PWD" >> ~/.zshrc
-echo "export PATH=\$PATH:\$ZERO_CLOUD/runtime/istio/bin" >> ~/.zshrc
-echo "## >>> Zero Cloud Environment End >>>" >> ~/.zshrc
-source ~/.zshrc
