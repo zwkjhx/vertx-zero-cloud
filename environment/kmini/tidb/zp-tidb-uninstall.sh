27dcdede271b4b4a20ff52ab826a93f520f4f834
Lang <silentbalanceyh@126.com>
2022-07-21 07:58:47 +0800

    Local

--- environment/kmini/tidb/zp-tidb-uninstall.sh
+++ environment/kmini/tidb/zp-tidb-uninstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-kubectl delete tidbmonitor $ZC_TIDB-monitor -n $ZC_TIDB_NS
+kubectl delete tidbmonitor $ZC_TIDB -n $ZC_TIDB_NS
 kubectl delete tc $ZC_TIDB -n $ZC_TIDB_NS
 kubectl delete tidbinitializer $ZC_TIDB-initializer -n $ZC_TIDB_NS
 kubectl delete pvc -n $ZC_TIDB_NS -l app.kubernetes.io/instance=$ZC_TIDB,app.kubernetes.io/managed-by=tidb-operator
